e574fced08e4134a571c7806b0fb057873b63cfb
Ary Demitropulos <demitropulosary@gmail.com>
2021-11-25 22:11:51 -0300

    Se agrega componente subtitulo

---
+++ src/app/components/subtitle/subtitle.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subtitle',
+  templateUrl: './subtitle.component.html',
+  styleUrls: ['./subtitle.component.css'],
+})
+export class SubtitleComponent implements OnInit {
+  @Input() text: string;
+  @Input() inverted?: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
